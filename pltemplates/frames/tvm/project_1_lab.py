c93b7e3b35c60f6d3748d925c2043c35bbd985c1
Nick DeRobertis <nick@claimfound.com>
2019-09-13 09:08:44 -0400

    Restructure lab 1 frame as it is used in two presentations

---
+++ pltemplates/frames/tvm/project_1_lab.py
@@ -0,0 +1,20 @@
+import pyexlatex as pl
+import pyexlatex.presentation as lp
+from pltemplates.labblock import LabBlock
+
+
+def get_project_1_lab_frame():
+    return lp.Frame(
+        [
+            LabBlock(
+                pl.UnorderedList([
+                    'Download and open the Project 1 document from Canvas',
+                    "It is up to you whether you want to attempt the Excel or Python model first",
+                    "If you feel comfortable with the Excel model, you may want to start with Python so I "
+                    "can give help on that today."
+                ]),
+                title='Project 1'
+            )
+        ],
+        title='Some Time to Work on the First Project'
+    )
