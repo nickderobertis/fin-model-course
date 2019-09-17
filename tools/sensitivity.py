9e196c5c33b42c2bf5874333315c84e63b49b3d9
Nick DeRobertis <whoopnip@gmail.com>
2019-09-17 08:12:20 -0400

    Label color bar in sensitivity plots. Complete docstring for sensitivity_hex_plots

--- tools/sensitivity.py
+++ tools/sensitivity.py
@@ -10,7 +10,8 @@ import numpy as np
 
 def sensitivity_hex_plots(sensitivity_values: Dict[str, Any], func: Callable,
                           result_name: str = 'Result', agg_func: Callable = np.mean,
-                          reverse_colors: bool = False, **func_kwargs) -> plt.Figure:
+                          reverse_colors: bool = False, grid_size: int = 8,
+                          **func_kwargs) -> plt.Figure:
     """
     Create hexbin plots showing how the func result varies with a passed dictionary of input values.
     Automatically creates a plot for each pair of input parameters passed.
@@ -19,11 +20,14 @@ def sensitivity_hex_plots(sensitivity_values: Dict[str, Any], func: Callable,
         values to use for that argument.
     :param func: Function that accepts arguments with names matching the keys of sensitivity_values, and outputs a
         scalar value.
-    :param result_name:
-    :param agg_func:
-    :param reverse_colors:
-    :param func_kwargs:
-    :return:
+    :param result_name: Name for result shown in graph color bar label
+    :param agg_func: If there are multiple results within the hex parameter area, function to aggregate those results to
+        get a single value for the hex area. The function should accept a sequence of values and return a scalar.
+    :param reverse_colors: Default is for red to represent low values of result, green for high values. Set
+        reverse_colors=True to have green represent low values of result and red for high values.
+    :param grid_size: Number of hex bins on each axis. E.g. passing 5 would create a 5x5 grid, 25 hex bins.
+    :param func_kwargs: Additional arguments to pass to func, regardless of the sensitivity values picked
+    :return: Sensitivity analysis hex bin sub plot figure
     """
     s_df = sensitivity_df(
         sensitivity_values,
@@ -37,7 +41,8 @@ def sensitivity_hex_plots(sensitivity_values: Dict[str, Any], func: Callable,
         sensitivity_cols,
         result_name=result_name,
         agg_func=agg_func,
-        reverse_colors=reverse_colors
+        reverse_colors=reverse_colors,
+        grid_size=grid_size
     )
 
 
@@ -59,7 +64,7 @@ def sensitivity_df(sensitivity_values: Dict[str, Any], func: Callable,
 
 def _hex_figure_from_sensitivity_df(df: pd.DataFrame, sensitivity_cols: Sequence[str],
                                     result_name: str = 'Result', agg_func: Callable = np.mean,
-                                    reverse_colors: bool = False) -> plt.Figure:
+                                    reverse_colors: bool = False, grid_size: int = 8) -> plt.Figure:
     color_map = _get_color_map(reverse_colors)
     num_columns = 3
     num_rows = int(math.ceil(len(sensitivity_cols) / num_columns))
@@ -67,14 +72,17 @@ def _hex_figure_from_sensitivity_df(df: pd.DataFrame, sensitivity_cols: Sequence
     fig = plt.figure(figsize=(15, 4 * num_rows))
     for i, (x, y) in enumerate(itertools.combinations(sensitivity_cols, 2)):
         ax = fig.add_subplot(gs[i])
-        df.plot.hexbin(x=x,
-                       y=y,
-                       ax=ax,
-                       C=result_name,
+        hb = ax.hexbin(x=df[x],
+                       y=df[y],
+                       C=df[result_name],
                        reduce_C_function=agg_func,
-                       gridsize=8,
-                       cmap=color_map,
-                       sharex=False)
+                       gridsize=grid_size,
+                       cmap=color_map)
+        plt.xlabel(x)
+        plt.ylabel(y)
+        cb = fig.colorbar(hb, ax=ax)
+        cb.set_label(result_name)
+    fig.tight_layout()
     return fig
 
 
