b7f729c430cc2a5bcd1f3a76dd0dd334ce0a2d4c
Nick DeRobertis <whoopnip@gmail.com>
2019-09-25 16:48:36 -0400

    Improve internal randomness graphic

--- pltemplates/graphics/internal_randomness.py
+++ pltemplates/graphics/internal_randomness.py
@@ -12,37 +12,33 @@ ALL_NODE_STYLE = [
 
 def internal_randomness_graphic() -> lg.TikZPicture:
     model_style = ['fill=orange!30'] + ALL_NODE_STYLE
+    middle_index = math.floor(NUM_MODEL_ROWS / 2)
 
     output_nodes = []
-    input_node = lg.Node('Inputs', options=_io_style(2))
-    model_node = lg.Node('Model', lg.Right(of=input_node), options=model_style)
-    im_arrow = lg.Arrow(input_node, model_node)
-    pic_contents = [
-        input_node,
-        model_node,
-        im_arrow
-    ]
+    pic_contents = []
     for i in range(NUM_MODEL_ROWS):
-        if i == 2:
-            output_position = lg.Right(of=model_node)
-        elif i == 0:
-            output_position = lg.Right(lg.Above(of=model_node))
+        if i == 0:
+            output_position = None
         else:
             output_position = lg.Below(of=output_node)  # below last output node
 
         output_node = lg.Node('Outputs', output_position, options=_io_style(i))
         output_nodes.append(output_node)
+        pic_contents.append(output_node)
 
-        mo_arrow = lg.Arrow(model_node, output_node)
-        pic_contents.extend([
-            output_node,
-            mo_arrow
-        ])
-    visualize_index = math.floor(NUM_MODEL_ROWS / 2)
-    visualize_node = lg.Node('Visualization', lg.Right(of=output_nodes[visualize_index]), options=model_style)
-    visualize_arrows = [lg.Arrow(out_node, visualize_node) for out_node in output_nodes]
-    pic_contents.append(visualize_node)
-    pic_contents.extend(visualize_arrows)
+    center_output = output_nodes[middle_index]
+    model_node = lg.Node('Model', lg.Left(of=center_output), options=model_style)
+    input_node = lg.Node('Inputs', lg.Left(of=model_node), options=_io_style(middle_index))
+
+    im_arrow = lg.Arrow(input_node, model_node)
+    output_arrows = [lg.Arrow(model_node, out_node) for out_node in output_nodes]
+    pic_contents.extend([
+        model_node,
+        input_node,
+        im_arrow,
+        *output_arrows
+    ])
+
     tp = lg.TikZPicture(pic_contents)
     return tp
 
