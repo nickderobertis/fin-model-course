79012e922399efd4190801d2f2d5d1a2fe82f963
Nick DeRobertis <whoopnip@gmail.com>
2019-10-16 18:28:14 -0400

    Full first version of project 2

--- plbuild/sources/document/2_debt_probability.py
+++ plbuild/sources/document/2_debt_probability.py
@@ -44,6 +44,10 @@ def get_content():
         default_prob_t_minus_1=('$p_{t-1}^{default}$', 0),
     )
 
+    align_c = lt.ColumnAlignment('c')
+    align_l = lt.ColumnAlignment('l')
+    align = lt.ColumnsAlignment([align_l, align_c])
+
     default_prob_eq_str = f'{outputs["default_prob_t"][0].strip("$")} = {outputs["default_prob_t_minus_1"][0].strip("$")} ' \
                           f'{inputs["default_prob_decay"][0].strip("$")}'
 
@@ -56,7 +60,7 @@ def get_content():
     {inputs["price_machine"][0]} to buy machinery. The business would like to borrow the funds for 
     {inputs["loan_life"][0]}, and at that time it will repay {inputs["price_machine"][0]} in full. Interest is 
     paid annually at a rate of {outputs["interest_rate"][0]} (in the final period, both 
-    {inputs["price_machine"][0]} and interest of {outputs["interest_rate"][0]} will be paid). As this is a small 
+    {inputs["price_machine"][0]} and interest at the rate of {outputs["interest_rate"][0]} will be paid). As this is a small 
     business, there is significant default risk, but that default risk decreases over time as the business 
     matures. The probability of default in the first year is {inputs["initial_default_prob"][0]}, and then each 
     year thereafter it is:
@@ -67,18 +71,13 @@ def get_content():
     pay a lot more in this period so there is a greater likelihood it can't come up with the funds. In the final year,
     (at year {inputs["loan_life"][0]}), the default probability is {inputs["final_default_prob"][0]}. 
     
-    When the business defaults during the life of the loan, before year {inputs["loan_life"][0]}, then the default
+    When the business defaults, then the default
     covenants of the loan trigger bankruptcy for the borrower, and the borrower must pay as much as it can on the loan 
-    in the bankruptcy process. The bankruptcy process takes 2 years, and then once it is resolved, the lender will 
+    in the bankruptcy process. The bankruptcy process takes two years, and then once it is resolved, the lender will 
     collect {inputs["recovery_rate"][0]}% of {inputs["price_machine"][0]}. For the year of default and the year after,
-    the lender will not collect any cash flows, and then 2 years after default, the lender will collect 
-    {pl.Equation(str_eq=recovery_eq_str)}. 
-    
-    When the business defaults at the end of the loan, the loan will be renegotiated. It will be extended by two years.
-    This means that in the original final year of the loan, and the year after, the business will pay the regular 
-    interest amount. Two years after the original repayment date, the business will be required to pay the original 
-    final payment of both {inputs["price_machine"][0]} and interest of {outputs["interest_rate"][0]}. At this time,
-    the business could default again, and the cycle would repeat (and this can keep going).
+    the lender will not collect any cash flows, and then two years after default, the lender will collect 
+    {pl.Equation(str_eq=recovery_eq_str)}. Note that this means the number of years of cash flows may be up to two
+    years greater than the life of the loan.
     """
 
     main_q_str = f"""
@@ -103,6 +102,18 @@ def get_content():
     Produce the same outputs as the main problem, but instead of evaluating 
     {pl.Equation(str_eq=default_prob_cases_eq_str)}, consider {inputs["initial_default_prob"][0]} as being normally
     distributed with mean {bonus_initial_default_prob_mean} and standard deviation {bonus_initial_default_prob_std}.
+    """
+
+    submission_str = f"""
+    You may choose to use Excel, Python, or a combination of the two for this model. If you are submitting a standalone
+    Excel or Python project, your submission will be the workbook or the notebook, respectively. If you are submitting
+    an xlwings project combining the two, then submit both the macro-enabled XLSM workbook and the .py Python file.
+    
+    If there is an Excel component to your project, please work off the "project_2_template.xlsm" and 
+    "project_2_template.py" (if needed). 
+    
+    If submitting a standalone Python ipynb notebook, please use the names {list(inputs.keys())} for your inputs and
+    place them in the second cell, and put your outputs in the bottom cell. Your notebook should run end-to-end.
     """
 
     return [
@@ -137,9 +148,59 @@ def get_content():
             ],
             title='Overview'
         ),
+        pl.Section(
+            [
+                pl.SubSection(
+                    [
+                        submission_str
+                    ],
+                    title='Submission'
+                ),
+                pl.SubSection(
+                    [
+                        pl.Center(
+                            lt.Tabular(
+                                [
+
+                                    lt.MultiColumn('Grading Breakdown', span=2),
+                                    lt.TopRule(),
+                                    lt.ValuesTable.from_list_of_lists(
+                                        [
+                                            ['Category', 'Percentage']
+                                        ]
+                                    ),
+                                    lt.TableLineSegment(0, 1),
+                                    lt.ValuesTable.from_list_of_lists(
+                                        [
+                                            ['Model Accuracy', '70%'],
+                                            ['Model Readability', '20%'],
+                                            ['Model Formatting', '10%'],
+                                            ['Bonus', '5%']
+                                        ]
+                                    ),
+                                    lt.MidRule(),
+                                    lt.ValuesTable.from_list_of_lists(
+                                        [
+                                            ['Total Possible', '105%']
+                                        ]
+                                    ),
+                                    lt.BottomRule()
+
+                                ],
+                                align=align
+                            )
+                        )
+                    ],
+                    title='Grading'
+                ),
+            ],
+            title='Submission & Grading'
+        )
 
     ]
 
-DOCUMENT_CLASS_KWARGS = {}
+DOCUMENT_CLASS_KWARGS = dict(
+    remove_section_numbering=True,
+)
 OUTPUT_NAME = TITLE
 
