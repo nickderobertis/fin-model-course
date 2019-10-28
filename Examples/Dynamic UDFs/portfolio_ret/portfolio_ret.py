f53fb918f38f45274be2cb4c7d544a2b8030c0ae
Nick DeRobertis <whoopnip@gmail.com>
2019-10-28 14:36:29 -0400

    Update python file for dynamic UDFs example

--- Examples/Dynamic UDFs/portfolio_ret/portfolio_ret.py
+++ Examples/Dynamic UDFs/portfolio_ret/portfolio_ret.py
@@ -1,3 +1,5 @@
+import random
+
 import xlwings as xw
 import pandas as pd
 
@@ -17,3 +19,14 @@ def portfolio_return(returns, weights):
     for i, ret in enumerate(returns):
         er = er + ret * weights[i]
     return er
+
+
+@xw.arg('num_rets', numbers=int)
+@xw.ret(expand='table')
+def random_normal_returns(mean, stdev, num_rets):
+    all_rets = []
+    for i in range(num_rets):
+        ret = random.normalvariate(mean, stdev)
+        all_rets.append(ret)
+    vertical_returns = [[ret] for ret in all_rets]
+    return vertical_returns
