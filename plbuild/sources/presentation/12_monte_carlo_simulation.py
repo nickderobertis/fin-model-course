e455eaa74be92b15459915248ab4d9f9ae542102
Nick DeRobertis <whoopnip@gmail.com>
2019-10-29 10:00:13 -0400

    Add section on formal monte carlo to presentation 12

--- plbuild/sources/presentation/12_monte_carlo_simulation.py
+++ plbuild/sources/presentation/12_monte_carlo_simulation.py
@@ -1,3 +1,4 @@
+import random
 import pyexlatex as pl
 import pyexlatex.table as lt
 import pyexlatex.presentation as lp
@@ -40,6 +41,35 @@ ORDER = 12
 def get_content():
     ev_bet = (999999/1000000) * 1 + (1/1000000) * (-750001)
     xlwings_mono = pl.Monospace('xlwings')
+    pd_mono = pl.Monospace('pandas')
+
+    ball_options = [
+        'fill',
+        'circle',
+        'inner sep=8pt'
+    ]
+
+    blue_ball_options = ball_options + [
+        'blue'
+    ]
+
+    red_ball_options = ball_options + [
+        'red'
+    ]
+
+    def rand_pos():
+        return random.randrange(-150, 150) / 100
+
+    blue_nodes = [lg.Node(None, (rand_pos(), rand_pos()), options=blue_ball_options) for _ in range(10)]
+    red_nodes = [lg.Node(None, (rand_pos(), rand_pos()), options=red_ball_options) for _ in range(10)]
+
+    red_blue_ball_graphic = lg.TikZPicture(
+        [
+            lg.Rectangle(5, 5, shape_options=['blue', 'thick']),
+            *blue_nodes,
+            *red_nodes
+        ]
+    )
 
     mc_python_lab = get_monte_carlo_python_exercise()
 
@@ -143,9 +173,187 @@ def get_content():
                 mc_python_lab.presentation_frames(),
             ],
             title='Running a First Monte Carlo Simulation',
-            short_title='Run MC'
+            short_title='Run MC',
         ),
+        pl.Section(
+            [
+                lp.Frame(
+                    [
+                        pl.TextSize(-2),
+                        'For the model given by:',
+                        pl.Equation(str_eq='y = f(X)', inline=False),
+                        pl.Equation(str_eq='X = [x_1, x_2, ..., x_n]', inline=False),
+                        pl.UnorderedList([
+                            [pl.Equation(str_eq='y:'), 'Model output'],
+                            [pl.Equation(str_eq='X:'), 'Model input matrix'],
+                            [pl.Equation(str_eq='x_i:'), 'Value of $i$th $x$ variable']
 
+                        ]),
+                        'To run $N$ Monte Carlo simulations, follow the following steps:',
+                        pl.OrderedList([
+                            ['Assign a probability distribution for each', pl.Equation(str_eq='x_i')],
+                            ['For each', pl.Equation(str_eq='x_i'),
+                             'randomly pick a value from its probability distribution. Store them as',
+                             pl.Equation(str_eq='X_j')],
+                            ['Repeat the previous step $N$ times, yielding',
+                             pl.Equation(str_eq='[X_1, X_2, ..., X_N]')],
+                            ['For each', pl.Equation(str_eq='X_j'), 'calculate', pl.Equation(str_eq='y_j = f(X_j)')],
+                            ['Store the values of', pl.Equation(str_eq='X_j'), 'mapped to', pl.Equation(str_eq='y_j')],
+                            ['Visualize and analyze', pl.Equation(str_eq='y_j'), 'versus', pl.Equation(str_eq='X_j')]
+                        ])
+                    ],
+                    title='Monte Carlo Simulation Process'
+                ),
+                lp.DimRevealListFrame(
+                    [
+                        'There are a multitude of outputs we can get from a Monte Carlo simulation. We saw a few '
+                        'already in the example.',
+                        [pl.Bold('Outcome probability distributions'), 'are the main output. We saw this with two '
+                         'approaches in the example, a', pl.Underline('histogram'), 'and a',
+                         pl.Underline('probability table.')],
+                        ['We also examined the', pl.Bold('probability of a certain outcome'), 'in whether we reached '
+                         'the desired cash.'],
+                        ['The last main output is examining the', pl.Bold('relationship between inputs and outputs.'),
+                         'for which common approaches include', pl.Underline('scatter plots'), 'and',
+                         pl.Underline('regressions.')]
+                    ],
+                    title='Outputs from Monte Carlo Simulation'
+                ),
+                lp.TwoColumnGraphicDimRevealFrame(
+                    [
+                        pl.TextSize(-1),
+                        'The outcome probability distribution represents the chance of receiving different '
+                        'outcomes from your model.',
+                        'There are two main ways to visualize a probability distribution: a plot and a table.',
+                        ['The plot, usually a', pl.Underline('histogram'), 'or', pl.Underline('KDE'),
+                         'gives a high-level overview of the probabilities and can uncover any non-normal '
+                         'features of the distribution.'],
+                        ['The probability table represents the chance of receiving the given value or '
+                         'lower.']
+                    ],
+                    graphics=[
+                        images_path('outcome-probability-distribution.png'),
+                        lt.Tabular(
+                            [
+
+                                lt.MultiColumn('Probability Table', span=2),
+                                lt.TopRule(),
+                                lt.ValuesTable.from_list_of_lists(
+                                    [
+                                        ['Probability', 'Value']
+                                    ]
+                                ),
+                                lt.TableLineSegment(0, 1),
+                                lt.ValuesTable.from_list_of_lists(
+                                    [
+                                        ['25%', '1020'],
+                                        ['50%', '1039'],
+                                        ['75%', '1053']
+                                    ]
+                                ),
+                                lt.BottomRule()
+
+                            ],
+                            align='c|c'
+                        )
+                    ],
+                    title='Outcome Probability Distributions',
+                    graphics_on_right=False,
+                ),
+                lp.TwoColumnGraphicDimRevealFrame(
+                    [
+                        'Imagine a box which contains red and blue balls. You do not know in advance how many there '
+                        'are of each color.',
+                        'You want to estimate the probability of getting a blue ball when pulling a ball from the box.',
+                        'To evaluate this, you grab a ball, write down its color, and put it back, 1,000 times.',
+                        'You pull a blue ball in 350 out of the 1,000 trials. What is the probability of getting blue?'
+                    ],
+                    graphics=[
+                        red_blue_ball_graphic
+                    ],
+                    title='Probability of a Certain Outcome - A Simple Example'
+                ),
+                lp.DimRevealListFrame(
+                    [
+                        'We followed the same logic when estimating the probability of receiving our desired cash '
+                        'in the investment example.',
+                        pl.Equation(str_eq=fr'p = \frac{{{pl.Text("Count of positive outcomes")}}}{{{pl.Text("Count of trials")}}}'),
+                        ['For the balls example, this is simply',
+                         pl.Equation(str_eq=r'p = \frac{350}{1000} = 0.35'),],
+                        ['In the investment example, we used', pd_mono, 'to check for each trial, whether it was a '
+                         'positive outcome (made it a 1) or not (made it a 0). Then the sum is the count of '
+                         'positive outcomes and so the mean is the probability.']
+                    ],
+                    title='Probability of a Certain Outcome, Formally'
+                ),
+                lp.DimRevealListFrame(
+                    [
+                        'Monte Carlo simulation can also provide a more comprehensive look at the relationship between '
+                        'inputs and outputs.',
+                        'While sensitivity analysis can be used to estimate the relationship between an input and '
+                        'output, it is usually done with other inputs at their base case',
+                        'The values of inputs may affect how other inputs affect the output. E.g. for the retirement '
+                        'model, an increase in interest rate increases wealth more if the initial salary was higher.',
+                        'As all the inputs change each time, you can get a more realistic view of the relationship, e.g. '
+                        'some trials with a higher interest rate will have high salary and some will have low salary.'
+                    ],
+                    title='Why Monte Carlo Simulations Help Understand Inputs vs. Outputs'
+                ),
+                lp.TwoColumnGraphicDimRevealFrame(
+                    [
+                        pl.TextSize(-1),
+                        'A scatter plot is a simple way to visualize the relationship between two variables',
+                        'If there is a relationship, you will see some defined pattern in the points. This may be '
+                        'somewhat of an upward or downward line (linear relationship) or some other shape such '
+                        'as a U (non-linear relationship).',
+                        'If there is no relationship, then there will just be a random cloud of points (lower '
+                        'plot) or a horizontal line.'
+                    ],
+                    graphics=[
+                        images_path('scatter-plot-line.png'),
+                        images_path('scatter-plot-no-relationship.png')
+                    ],
+                    graphics_on_right=False,
+                    title='Visualizing the Relationship between Inputs and Outputs'
+                ),
+                lp.TwoColumnGraphicDimRevealFrame(
+                    [
+                        pl.TextSize(-2),
+                        'The scatter plots help give a broad understanding of the relationship but do not answer the '
+                        'question, how much will my output change if my input changes? E.g. if I earn 10,000 more '
+                        'for a starting salary, how much sooner can I retire?',
+                        'Simply increasing the input in your model and checking the output is not enough, because it '
+                        'does not take into account how all the other variables may be changing.',
+                        'Multivariate regression is a general tool which is good at answering these kinds of questions, '
+                        'while taking into account all the changing inputs.'
+                    ],
+                    graphics=[
+                        images_path('excel-multivariate-reg.png')
+                    ],
+                    title='Numerically Analyzing the Relationships'
+                ),
+                lp.DimRevealListFrame(
+                    [
+                        pl.TextSize(-1),
+                        'The coefficient in a multivariate regression represents how much the outcome variable '
+                        'changes with a one unit change in the input variable.',
+                        'E.g. a coefficient of -.0002 on starting salary in explaining years to retirement would mean '
+                        r'that a \$1 increase in starting salary is associated with a decrease in years to retirement by .0002 years, or '
+                        r'a \$10,000 increase in starting salary is associated with a decrease in years to retirement by 2 years.',
+                        'All interpretations are "all else constant", meaning that it does not consider relationships '
+                        'between the inputs. E.g. if starting salary is higher because of a good economy, and interest '
+                        'rates are also higher due to the good economy, the starting salary coefficient is not taking '
+                        'into account the increase in interest rates.',
+                        'Be careful about units. If you use decimals for percentages, you will need to multiply or '
+                        'divide by 100 to get the effect in percentages.'
+                    ],
+                    title='How to use Multivariate Regression'
+                )
+
+            ],
+            title='A More Formal Treatment of Monte Carlo Simulation',
+            short_title='Formal MC',
+        ),
         lp.Appendix(
             [
                 mc_python_lab.appendix_frames(),
