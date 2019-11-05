62cc6c54f6adf6a37fa43086171dc4c7927d521f
Nick DeRobertis <whoopnip@gmail.com>
2019-11-05 07:58:41 -0500

    Modify lab exercise to make answer slides orange and link to answer only appears for that slide and not all other slides

--- pltemplates/exercises/lab_exercise.py
+++ pltemplates/exercises/lab_exercise.py
@@ -36,12 +36,17 @@ class LabExercise(pl.Template):
         return self.contents[1:]
 
     def _get_content(self):
-        all_refs = self._get_all_references()
+        question_refs = self._get_question_references()
+        answer_refs = self._get_answers_references()
+
         all_content = []
         # Handle exercise slides
         for i, bullet_content in enumerate(self.all_bullet_content):
-            refs = deepcopy(all_refs)
+            refs = deepcopy(question_refs)
             refs.pop(i)  # remove reference to this item
+            # Add answer ref specifically for this item
+            if answer_refs[i] is not None:
+                refs.append(answer_refs[i])
             disp_idx = i + 1
             level_str = f', Level {disp_idx}'
             block_title = self.block_title + level_str
@@ -51,21 +56,17 @@ class LabExercise(pl.Template):
             all_content.append(lab_frame)
         # Handle answer slides
         for i, bullet_content in enumerate(self.answers_content):
-            if not bullet_content:
-                # Didn't get answers for this slide
-                continue
-            refs = deepcopy(all_refs)
-            refs.pop(i + len(self.all_bullet_content))  # remove reference to this item
+            refs = deepcopy(question_refs)
             disp_idx = i + 1
             level_str = f', Answers for Level {disp_idx}'
             block_title = self.block_title + level_str
             frame_title = self.frame_title + level_str
             label = self._get_label_for(i, answers=True)
-            lab_frame = LabFrame(bullet_content, block_title, frame_title, bottom_content=refs, label=label)
+            lab_frame = LabFrame(bullet_content, block_title, frame_title, bottom_content=refs, label=label, color='orange')
             all_content.append(lab_frame)
         return all_content
 
-    def _get_all_references(self):
+    def _get_question_references(self):
         refs = []
         # Handle exercise slides
         for i, bullet_content in enumerate(self.all_bullet_content):
@@ -76,11 +77,14 @@ class LabExercise(pl.Template):
             color_ref = f'Slide {pl.TextColor(pl.Underline(ref), "blue")}'
             full_ref = f'{level_str}: {color_ref}'
             refs.append(full_ref)
-        # Handle answer slides
+        return refs
+
+    def _get_answers_references(self):
+        refs = []
         for i, bullet_content in enumerate(self.answers_content):
             if not bullet_content:
                 # Didn't get answers for this slide
-                continue
+                refs.append(None)
             disp_idx = i + 1
             level_str = f'Answers {disp_idx}'
             label = self._get_label_for(i, answers=True)
