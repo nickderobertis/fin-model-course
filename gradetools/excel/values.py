512ba4be2944491c9029714e0632bee32fd5b6a4
Nick DeRobertis <whoopnip@gmail.com>
2019-11-11 17:57:01 -0500

    Better way of getting all results for each project 2 calculation

--- gradetools/excel/values.py
+++ gradetools/excel/values.py
@@ -16,6 +16,24 @@ def get_range_value(cell: str, retries=10):
             time.sleep(.1)
 
 
+def get_non_none_range_value(cell: str, retries=10):
+    val = None
+    while True:
+        if retries < 0:
+            raise ValueError(f'could not get non-none value for {cell} after retrying')
+        try:
+            val = xw.Range(cell).value
+        except com_error:
+            pass
+        if val is not None:
+            return val
+
+        # Retry whether com_error was hit or None value was pulled
+        retries -= 1
+        time.sleep(.1)
+
+
+
 def set_range_value(value: Any, cell: str, retries=10):
     while True:
         if retries < 0:
--- gradetools/project_2/calculate.py
+++ gradetools/project_2/calculate.py
@@ -4,7 +4,7 @@ import time
 from pythoncom import com_error
 import xlwings as xw
 
-from gradetools.excel.values import get_range_value
+from gradetools.excel.values import get_non_none_range_value
 
 
 def calculate_app_extract_irrs(n_iter: int = 3, out_cell: str = 'B15') -> List[float]:
@@ -14,16 +14,16 @@ def calculate_app_extract_irrs(n_iter: int = 3, out_cell: str = 'B15') -> List[f
     iter_count = 0
     while len(values) < n_iter:
         iter_count += 1
-        try:
-            app.api.CalculateFull()
-            value = get_range_value(out_cell)
-        except com_error:
-            continue
-        if value is None:
-            continue
-        values.append(value)
-        if iter_count > n_iter * 10:
-            # With 10x as many iterations as should be needed, still haven't extracted enough values, must have issue
+        if iter_count > n_iter * 5:
+            # With 5x as many iterations as should be needed, still haven't extracted enough values, must have issue
             raise ValueError(f'could not get enough results from {out_cell}. Have {len(values)} results '
                              f'after {iter_count} iterations')
+        try:
+            app.api.CalculateFull()
+        except com_error:
+            time.sleep(.1)
+            continue
+        value = get_non_none_range_value(out_cell)
+        values.append(value)
+
     return values
