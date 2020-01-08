f9a42144e32f4f302ee505c7e2fd6bef88a7b221
Nick DeRobertis <whoopnip@gmail.com>
2020-01-08 09:39:24 -0500

    Update slides 1 for sections, and update model flowchart to reduce spacing to fit with sections

--- plbuild/sources/presentation/1_intro.py
+++ plbuild/sources/presentation/1_intro.py
@@ -78,182 +78,200 @@ def get_content():
     interpreter_mono = Monospace('>>>')
 
     return [
-        TwoColumnGraphicDimRevealFrame(
+        pl.Section(
             [
-                'This is a skills-based course focused on teaching financial modeling techniques in Python and Excel',
-                'The focus is not a lot of specific models, but rather general model-building techniques',
-                'The focus will be simple models, but extending them in powerful ways'
-            ],
-            graphics=[
-                images_path('python-logo.png'),
-                images_path('excel-logo.png')
-            ],
-            title='What is this Class?'
-        ),
-        GraphicFrame(
-            input_model_output_fc,
-            title='What is a Model?'
-        ),
-        ModelFlowchartFrame(
-            [
-                [
-                    'Wages, Savings',
-                    'Investment',
-                    Node('Cash in the bank, person retires',
-                         options=real_world_style + ['text width=2.5cm'])
-                ],
-                [
-                    Node('Cash Flows, Savings Rate, Interest Rates',
-                         options=model_style + ['text width=3.5cm']),
-                    'Model',
-                    Node('FV of CF, time until retirement', options=model_style + ['text width=2.5cm'])
-                ]
-            ],
-            title='A Retirement Problem'
-        ),
-        ModelFlowchartFrame(
-            [
-                [
-                    Node('Microsoft creates software', options=real_world_style + ['text width=2cm']),
-                    'Sells the software',
-                    Node('Generates cash for investors, stock reaches a price',
-                         options=real_world_style + ['text width=3cm'])
-                ],
-                [
-                    Node('Revenue, COGS, SG&A, growth rates, costs of capital, etc.',
-                         options=model_style + ['text width=3.5cm']),
-                    'Model',
-                    Node('Stock price, stock returns', options=model_style + ['text width=2.5cm'])
-
-                ]
-            ],
-            title='Valuing a Company (DCF Model)'
-        ),
-        GraphicFrame(
-            images_path('xkcd-python.png'),
-            title='Why Python?'
-        ),
-        Frame(
-            [
-                UnorderedList(
+                TwoColumnGraphicDimRevealFrame(
                     [
-                        DimAndRevealListItems(
-                            [
-                                "The easiest to learn mainstream programming language",
-                                'Heavily used in the financial industry'
-                            ],
-                            dim_last_item=True,
-                            vertical_fill=True
-                        )
-                    ]
+                        'This is a skills-based course focused on teaching financial modeling techniques in Python and Excel',
+                        'The focus is not a lot of specific models, but rather general model-building techniques',
+                        'The focus will be simple models, but extending them in powerful ways'
+                    ],
+                    graphics=[
+                        images_path('python-logo.png'),
+                        images_path('excel-logo.png')
+                    ],
+                    title='What is this Class?'
                 ),
-                VFill(),
-                Block(
-                    MultiCol([
-                        UnorderedList([
-                            'Modeling',
-                            'Data science',
-                            'Algorithmic Trading',
-                            'Scripting',
-                        ]),
-                        UnorderedList([
-                            'Devices',
-                            'Web Development',
-                            'Web Scraping',
-                            'Even these slides',
-                        ])
-                    ]),
-                    title='Python is the Most Flexible of the Top Languages',
-                    overlay=next_slide_ov
-                )
-            ],
-            title='Python is a Good Choice for Finance'
-        ),
-        GraphicFrame(
-            images_path('python-popularity.PNG'),
-            title='Python is the Fastest Growing Programming Lanugage'
-        ),
-        DimRevealListFrame(
-            [
-                "Open source - completely free and open",
-                "Focus on readability - almost pseudo-code",
-                "Take as much as you need. Easy for beginners, many features for experts.",
-                "Deep integrations with Excel - VBA replacement, run Python in Excel, run Excel from Python"
-            ],
-            title="More Python Advantages",
-        ),
-        GraphicFrame(
-            images_path('python-terminal.png'),
-            title='Why Not use Python?'
-        ),
-        DimRevealListFrame(
-            [
-                "No graphical interface (by default)",
-                "Hard to get others working with it if they don't know Python",
-                "Can take more work to get started on a project"
-            ],
-            title="Python Disadvantages"
-        ),
-        GraphicFrame(
-            images_path('vba-terminal.png'),
-            title='Why Not use VBA?'
-        ),
-        TwoColumnGraphicDimRevealFrame(
-            [
-                "Code is not as readable as Python",
-                "Power is limited to working within Microsoft Office",
-                "Python has a complete VBA API built into a package - Python can do VBA and more"
-            ],
-            [
-                images_path('vba-logo.png')
-            ],
-            title="VBA is Old-School"
-        ),
-        DimRevealListFrame(
-            [
-                "Excel is everywhere. Most of the world's data is in Excel spreadsheets",
-                '(Nearly) everyone knows how use it',
-                "You can see what you're doing (without effort)",
-                "Easy introspection into a particular value"
-            ],
-            title="We Can't Ditch Excel Yet"
-        ),
-        GraphicFrame(
-            images_path('excel-hell.jpg'),
-            title='Escaping Excel Hell'
-        ),
-        DimRevealListFrame(
-            [
-                "Code and view are mixed together, code is hidden",
-                "Both cell formulas and VBA macros - What is going on?",
-                "Easy to make mistakes (one cell different)",
-                "Some tasks which are very simple in Python are very complex in Excel"
-            ],
-            title="The Pains of Excel"
-        ),
-        Frame(
-            [
-                LabBlock(
-                    OrderedList(
+                GraphicFrame(
+                    input_model_output_fc,
+                    title='What is a Model?'
+                ),
+                ModelFlowchartFrame(
+                    [
                         [
-                            f'Go to {anaconda_link} to download Python 3.7',
-                            'Follow the steps in the installer',
-                            'You will hit "Advanced Installation Options". It is very important that you select "Add '
-                            'Anaconda to my PATH environment variable". It says it is not recommended, and will '
-                            'highlight it in red when checked, but we will need it later in the course.',
-                            'Open CMD (windows key, search cmd)',
-                            f'Type {mono_python} and hit enter. You should see Python 3.7 and a {interpreter_mono} come up.'
-                        ]
-                    ),
-                    title='Install Steps',
+                            'Wages, Savings',
+                            'Investment',
+                            Node('Cash in the bank, person retires',
+                                 options=real_world_style + ['text width=2.5cm'])
+                        ],
+                        [
+                            Node('Cash Flows, Savings Rate, Interest Rates',
+                                 options=model_style + ['text width=3.5cm']),
+                            'Model',
+                            Node('FV of CF, time until retirement', options=model_style + ['text width=2.5cm'])
+                        ],
+                    ],
+                    title='A Retirement Problem',
                 ),
-                AlertBlock(
-                    'Make sure you have selected Python 3.7 and not 2.7'
+                ModelFlowchartFrame(
+                    [
+                        [
+                            Node('Microsoft creates software', options=real_world_style + ['text width=2cm']),
+                            'Sells the software',
+                            Node('Generates cash for investors, stock reaches a price',
+                                 options=real_world_style + ['text width=3cm'])
+                        ],
+                        [
+                            Node('Revenue, COGS, SG&A, growth rates, costs of capital, etc.',
+                                 options=model_style + ['text width=3.5cm']),
+                            'Model',
+                            Node('Stock price, stock returns', options=model_style + ['text width=2.5cm'])
+
+                        ]
+                    ],
+                    title='Valuing a Company (DCF Model)'
+                ),
+            ],
+            title='Introduction to Financial Modeling',
+            short_title='Intro'
+        ),
+        pl.Section(
+            [
+                GraphicFrame(
+                    images_path('xkcd-python.png'),
+                    title='Why Python?'
+                ),
+                Frame(
+                    [
+                        UnorderedList(
+                            [
+                                DimAndRevealListItems(
+                                    [
+                                        "The easiest to learn mainstream programming language",
+                                        'Heavily used in the financial industry'
+                                    ],
+                                    dim_last_item=True,
+                                    vertical_fill=True
+                                )
+                            ]
+                        ),
+                        VFill(),
+                        Block(
+                            MultiCol([
+                                UnorderedList([
+                                    'Modeling',
+                                    'Data science',
+                                    'Algorithmic Trading',
+                                    'Scripting',
+                                ]),
+                                UnorderedList([
+                                    'Devices',
+                                    'Web Development',
+                                    'Web Scraping',
+                                    'Even these slides',
+                                ])
+                            ]),
+                            title='Python is the Most Flexible of the Top Languages',
+                            overlay=next_slide_ov
+                        )
+                    ],
+                    title='Python is a Good Choice for Finance'
+                ),
+                GraphicFrame(
+                    images_path('python-popularity.PNG'),
+                    title='Python is the Fastest Growing Programming Lanugage'
+                ),
+                DimRevealListFrame(
+                    [
+                        "Open source - completely free and open",
+                        "Focus on readability - almost pseudo-code",
+                        "Take as much as you need. Easy for beginners, many features for experts.",
+                        "Deep integrations with Excel - VBA replacement, run Python in Excel, run Excel from Python"
+                    ],
+                    title="More Python Advantages",
+                ),
+                GraphicFrame(
+                    images_path('python-terminal.png'),
+                    title='Why Not use Python?'
+                ),
+                DimRevealListFrame(
+                    [
+                        "No graphical interface (by default)",
+                        "Hard to get others working with it if they don't know Python",
+                        "Can take more work to get started on a project"
+                    ],
+                    title="Python Disadvantages"
+                ),
+                GraphicFrame(
+                    images_path('vba-terminal.png'),
+                    title='Why Not use VBA?'
+                ),
+                TwoColumnGraphicDimRevealFrame(
+                    [
+                        "Code is not as readable as Python",
+                        "Power is limited to working within Microsoft Office",
+                        "Python has a complete VBA API built into a package - Python can do VBA and more"
+                    ],
+                    [
+                        images_path('vba-logo.png')
+                    ],
+                    title="VBA is Old-School"
+                ),
+                DimRevealListFrame(
+                    [
+                        "Excel is everywhere. Most of the world's data is in Excel spreadsheets",
+                        '(Nearly) everyone knows how use it',
+                        "You can see what you're doing (without effort)",
+                        "Easy introspection into a particular value"
+                    ],
+                    title="We Can't Ditch Excel Yet"
+                ),
+                GraphicFrame(
+                    images_path('excel-hell.jpg'),
+                    title='Escaping Excel Hell'
+                ),
+                DimRevealListFrame(
+                    [
+                        "Code and view are mixed together, code is hidden",
+                        "Both cell formulas and VBA macros - What is going on?",
+                        "Easy to make mistakes (one cell different)",
+                        "Some tasks which are very simple in Python are very complex in Excel"
+                    ],
+                    title="The Pains of Excel"
+                ),
+                Frame(
+                    [
+                        LabBlock(
+                            [
+                                pl.TextSize(-1),
+                                OrderedList(
+                                    [
+                                        f'Go to {anaconda_link} to download Python 3.7',
+                                        'Follow the steps in the installer',
+                                        'You will hit "Advanced Installation Options". It is very important that you select "Add '
+                                        'Anaconda to my PATH environment variable". It says it is not recommended, and will '
+                                        'highlight it in red when checked, but we will need it later in the course.',
+                                        'Open CMD (windows key, search cmd)',
+                                        f'Type {mono_python} and hit enter. You should see Python 3.7 and a {interpreter_mono} come up.'
+                                    ]
+                                )
+                            ],
+                            title='Install Steps',
+                        ),
+                        AlertBlock(
+                            'Make sure you have selected Python 3.7 and not 2.7'
+                        )
+                    ],
+                    title="Let's Get Python Set Up on your System"
                 )
             ],
-            title="Let's Get Python Set Up on your System"
-        )
+            title='Introduction to the Modeling Toolset',
+            short_title='Tools and Skills',
+        ),
     ]
 
-DOCUMENT_CLASS_KWARGS = {}
+DOCUMENT_CLASS_KWARGS = dict(
+    nav_header=True,
+    toc_sections=True
+)
 OUTPUT_NAME = TITLE
