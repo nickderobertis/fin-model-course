f3f5c98440bca0668c252b8fef173a82b788178e
Nick DeRobertis <whoopnip@gmail.com>
2020-02-26 13:37:36 -0500

    Update project 1 cases to use new python model with new inputs

--- fin_model_course/gradetools/project_1/cases.py
+++ fin_model_course/gradetools/project_1/cases.py
@@ -1,4 +1,7 @@
-from models.project_1 import PhoneManufacturingModel
+from typing import Union, Dict, Sequence
+
+from gradetools.project_1.config import PROJECT_1_NOTEBOOK_PATH
+from gradetools.py.execute2.main import read_notebook_and_run_extracting_globals, ReplacementConfig
 
 INPUT_CASES = [
     dict(
@@ -7,9 +10,11 @@ INPUT_CASES = [
         n_life=10,
         price_phone=500,
         price_scrap=50000,
-        d_0=100000,
+        d_1=100000,
         g_d=0.2,
-        interest=0.05
+        interest=0.05,
+        cost_machine_adv=1000000,
+        cogs_phone=250,
     ),
     dict(
         n_phones=1000000,
@@ -17,9 +22,11 @@ INPUT_CASES = [
         n_life=5,
         price_phone=200,
         price_scrap=30000,
-        d_0=1000000,
+        d_1=1000000,
         g_d=0.25,
-        interest=0.04
+        interest=0.04,
+        cost_machine_adv=2000000,
+        cogs_phone=200,
     ),
     dict(
         n_phones=10000,
@@ -27,9 +34,11 @@ INPUT_CASES = [
         n_life=18,
         price_phone=2000,
         price_scrap=300000,
-        d_0=100000,
+        d_1=100000,
         g_d=0.15,
-        interest=0.06
+        interest=0.06,
+        cost_machine_adv=500000,
+        cogs_phone=10,
     ),
     dict(
         n_phones=1000,
@@ -37,17 +46,20 @@ INPUT_CASES = [
         n_life=2,
         price_phone=20,
         price_scrap=300000,
-        d_0=100000,
+        d_1=100000,
         g_d=0.1,
-        interest=0.1
+        interest=0.1,
+        cost_machine_adv=10000,
+        cogs_phone=500,
     )
 ]
 
-def output_dict_from_input_dict(input_dict):
-    model = PhoneManufacturingModel(**input_dict)
+def output_dict_from_input_dict(input_dict: Dict[str, Union[int, float]]):
+    rc = ReplacementConfig('model_data', 'ModelInputs', kwargs=input_dict)
+    globs = read_notebook_and_run_extracting_globals(PROJECT_1_NOTEBOOK_PATH, replacements=[rc], suppress_output=True)
     correct_values = dict(
-        pv=model.pv,
-        cash_flows=model.cash_flows
+        pv=globs['npv'],
+        cash_flows=globs['cash_flows']
     )
     return correct_values
 
---
+++ fin_model_course/gradetools/project_1/config.py
@@ -0,0 +1,5 @@
+import os
+
+PROJECT_1_NOTEBOOK_FOLDER = os.path.sep.join(['Projects', 'Project 1', 'Python'])
+PROJECT_1_NOTEBOOK_NAME = 'Project 1 - Machines Capital Budgeting.ipynb'
+PROJECT_1_NOTEBOOK_PATH = os.path.join(PROJECT_1_NOTEBOOK_FOLDER, PROJECT_1_NOTEBOOK_NAME)
