a5abe2c67855b7396edd92f0ea7df7845abf9dec
Nick DeRobertis <whoopnip@gmail.com>
2020-02-26 13:38:08 -0500

    Update project 1 excel config for new template

--- fin_model_course/gradetools/project_1/excel/config.py
+++ fin_model_course/gradetools/project_1/excel/config.py
@@ -1,12 +1,14 @@
 INPUT_RANGE_DICT = dict(
     n_phones='B4',
-    n_machines='B14',
-    n_life='B6',
-    price_phone='B9',
+    n_machines='B16',
+    n_life='B7',
+    price_phone='B10',
     price_scrap='B5',
-    d_0='B10',
-    g_d='B11',
-    interest='B17'
+    d_1='B12',
+    g_d='B13',
+    interest='B19',
+    cost_machine_adv='B6',
+    cogs_phone='B11',
 )
 
 OUTPUT_RANGE_DICT = dict(
