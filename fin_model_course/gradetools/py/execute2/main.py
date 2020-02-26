b164cedfe8c086ca1adb04a0e737359bcdf022cc
Nick DeRobertis <whoopnip@gmail.com>
2020-02-26 13:20:17 -0500

    Fix replacer and add main function to run notebook with replacements and extract globals

--- fin_model_course/gradetools/py/execute2/main.py
+++ fin_model_course/gradetools/py/execute2/main.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
-from typing import Optional, Sequence, Union, Dict
+from typing import Optional, Sequence, Union, Dict, Any
 
 import nbformat
 
 from gradetools.py.execute2.gen_ast import create_ast_function_call_with_numeric_values
 from gradetools.py.execute2.nbsource import source_from_notebook_node
 from gradetools.py.execute2.replace import replace_in_source
+from gradetools.py.execute2.run import run_source_extract_globals
 
 
 @dataclass
@@ -18,10 +19,15 @@ class ReplacementConfig:
         self.ast_call = create_ast_function_call_with_numeric_values(self.function_name, **self.kwargs)
 
 
-def read_notebook_and_run_extracting_globals(notebook_path: str, replacements: Optional[Sequence[ReplacementConfig]] = None):
+def read_notebook_and_run_extracting_globals(
+    notebook_path: str,
+    replacements: Optional[Sequence[ReplacementConfig]] = None,
+    suppress_output: bool = False
+) -> Dict[str, Any]:
     nb = nbformat.read(notebook_path, as_version=4)
     source = source_from_notebook_node(nb)
     if replacements is not None:
         for config in replacements:
             source = replace_in_source(source, config.assign_name, config.ast_call)
-    # WIP
+    globs = run_source_extract_globals(source, suppress_output=suppress_output)
+    return globs
--- fin_model_course/gradetools/py/execute2/replace.py
+++ fin_model_course/gradetools/py/execute2/replace.py
@@ -1,21 +1,6 @@
 import ast
 import astor
 
-# TEMP - test script
-"""
-from gradetools.py.execute2.replace import replace_in_source
-from gradetools.py.execute2.nbsource import source_from_notebook_node
-from gradetools.py.execute2.gen_ast import create_ast_function_call_with_numeric_values
-import nbformat
-import ast
-
-value = create_ast_function_call_with_numeric_values('ModelInputs', n_phones=100, price_scrap=200)
-nb = nbformat.read(notebook_path, as_version=4)
-source = source_from_notebook_node(nb)
-new_source = replace_in_source(source, 'model_data', value)
-print(new_source)
-"""
-
 
 def replace_in_source(source: str, target_assign_name: str, replace_with: ast.AST):
     mod = ast.parse(source)
@@ -31,7 +16,8 @@ class AstAssignReplacer(ast.NodeTransformer):
         self.replace_with = replace_with
 
     def visit_Assign(self, node: ast.Assign):
-        if node.targets[0] == self.target_assign_name:
+        main_target = node.targets[0]
+        if hasattr(main_target, 'id') and main_target.id == self.target_assign_name:
             # Got the targeted assign, replace
             return ast.copy_location(
                 ast.Assign(
