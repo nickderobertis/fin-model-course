128340bd09ca55e970b9dde878bb20028d22d9e5
Nick DeRobertis <whoopnip@gmail.com>
2020-09-01 10:07:24 -0400

    Add models for lecture information

---
+++ fin_model_course/lectures/model.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+from typing import Union, Sequence, Type, T, Optional
+
+import pyexlatex as pl
+
+
+@dataclass
+class LectureNotes:
+    items: Sequence[Union[str, "LectureNotes"]]
+    title: str
+
+    def __getitem__(self, item):
+        return self.items[item]
+
+    def to_models(
+        self, top_model: Type[T] = pl.Section, sub_model: Type = pl.UnorderedList
+    ) -> T:
+        items = []
+        for item in self:
+            if isinstance(item, self.__class__):
+                items.append(item.to_models(top_model=sub_model, sub_model=sub_model))
+            else:
+                items.append(item)
+
+        # Collect strs into sub models
+        final_items = []
+        current_str_items = []
+        for item in items:
+            if isinstance(item, str):
+                current_str_items.append(item)
+            else:
+                # Not a str item, so the str section has ended
+                if current_str_items:
+                    # Add collection of the str items and wipe str items
+                    final_items.append(sub_model(current_str_items))
+                    current_str_items = []
+                # Now add the current not str item
+                final_items.append(item)
+
+        # If str items were last, will have some left over
+        if current_str_items:
+            final_items.append(sub_model(current_str_items))
+
+        return top_model(final_items, title=self.title)
+
+
+@dataclass
+class Lecture:
+    title: str
+    notes: LectureNotes
+    video_url: Optional[str] = None
+
+
+@dataclass
+class LectureGroup:
+    title: str
+    lectures: Sequence[Lecture]
+
+    def __getitem__(self, item):
+        return self.lectures[item]
+
+    def to_models(
+            self, top_model: Type[T] = pl.Section, sub_model: Type = pl.UnorderedList
+    ) -> T:
+        return [mod.notes.to_models(top_model=top_model, sub_model=sub_model) for mod in self]
