35afc6457124910f07da8d15a585da5a49e24e0d
Nick DeRobertis <whoopnip@gmail.com>
2020-09-07 03:43:36 -0400

    Add lecture 3 to lectures config

--- fin_model_course/lectures/config.py
+++ fin_model_course/lectures/config.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from lectures.dynamic_excel.main import get_dynamic_salary_excel_lecture
 from lectures.intro.main import get_intro_lecture
 from lectures.model import LectureGroup
 from lectures.start_python_excel.main import get_getting_started_with_python_and_excel_lecture
@@ -9,4 +10,5 @@ def get_lecture_groups() -> List[LectureGroup]:
     return [
         get_intro_lecture(),
         get_getting_started_with_python_and_excel_lecture(),
+        get_dynamic_salary_excel_lecture(),
     ]
