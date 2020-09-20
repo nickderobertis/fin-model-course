22e0043c805ec2070846e29d43524d55b1d3707c
Nick DeRobertis <whoopnip@gmail.com>
2020-09-20 11:37:03 -0400

    Add shell for lecture 7

--- fin_model_course/lectures/config.py
+++ fin_model_course/lectures/config.py
@@ -7,6 +7,7 @@ from lectures.lab_exercises.main import get_lab_exercises_lecture
 from lectures.model import LectureGroup
 from lectures.projects.main import get_projects_lecture
 from lectures.python_basics.main import get_python_basics_lecture
+from lectures.sensitivity_analysis.main import get_sensitivity_analysis_lecture
 from lectures.start_python_excel.main import get_getting_started_with_python_and_excel_lecture
 from lectures.visualization.main import get_visualization_lecture
 
@@ -19,6 +20,7 @@ def get_lecture_groups(include_labs: bool = True, include_projects: bool = True)
         get_python_basics_lecture(),
         get_dynamic_salary_python_lecture(),
         get_visualization_lecture(),
+        get_sensitivity_analysis_lecture(),
     ]
 
     if not include_labs and not include_projects:
---
+++ fin_model_course/lectures/sensitivity_analysis/main.py
@@ -0,0 +1,24 @@
+from lectures.sensitivity_analysis import notes
+from lectures.model import LectureGroup
+from resources.models import RESOURCES
+from schedule.main import LECTURE_7_NAME
+
+
+def get_sensitivity_analysis_lecture() -> LectureGroup:
+    lecture_index = 7
+    title = LECTURE_7_NAME
+    description = 'Expand models to check the full range of inputs and see how it impacts the outputs.'
+    resources = [
+        *RESOURCES.lectures.sensitivity_analysis.resources(),
+    ]
+    lectures = [
+        notes.get_parameter_exploration_intro_lecture(),
+        notes.get_sensitivity_analysis_intro_lecture(),
+        notes.get_sensitivity_analysis_excel_lecture(),
+        notes.get_python_dictionaries_lecture(),
+        notes.get_python_list_comprehensions_lecture(),
+        notes.get_python_imports_lecture(),
+        notes.get_python_sensitivity_analysis_lecture(),
+        notes.get_python_sensitivity_analysis_retirement_lecture(),
+    ]
+    return LectureGroup(title, description, lectures, order=lecture_index, global_resources=resources)
