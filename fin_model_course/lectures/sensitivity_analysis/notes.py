0b4b577faf72e99c701d3f7ef110aa9c5b2d2bbb
Nick DeRobertis <whoopnip@gmail.com>
2020-09-21 06:59:11 -0400

    Add real python imports resource

--- fin_model_course/lectures/sensitivity_analysis/notes.py
+++ fin_model_course/lectures/sensitivity_analysis/notes.py
@@ -77,6 +77,10 @@ def get_python_imports_lecture() -> Lecture:
     ], title=title)
     resources = [
         RESOURCES.examples.intro.python.dicts_list_comp_imports_notebook,
+        LectureResource(
+            'Guide to Python Imports',
+            external_url='https://realpython.com/absolute-vs-relative-python-imports/'
+        )
     ]
     return Lecture(title, week_covered, notes, youtube_id=youtube_id, resources=resources)
 
